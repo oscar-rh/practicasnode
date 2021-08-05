6fc2d3a3820cf339779f9285edbaddead117e0f2
oscar-rh <oscar.rodriguezh@gmail.com>
2021-08-05 16:33:40 -0500

    practica de express mentors y koders en miproyectoexpress

--- miproyectoexpress/index.js
+++ miproyectoexpress/index.js
@@ -24,21 +24,35 @@ server.post('/koders1', (request, response) => {
 })
 
 server.get('/mentors', (request, response) => {
-    response.setHeader("Content-type", "application/json")
-    response.write(JSON.stringify({mensajeGet: "Aqui encontraras a los mentores de kodemia"}))
-    response.end()
+
+
+    readFilePromise('mentors.json')
+    .then( (content) =>{
+        const contentObject = JSON.parse(content)
+        response.json({contentObject})
+    })
+    .catch((err) => {
+        response.status(400).json({error: "No se pudo leer el archivo"})
+    })
+
+    // response.setHeader("Content-type", "application/json")
+    // response.write(JSON.stringify({mensajeGet: "Aqui encontraras a los mentores de kodemia"}))
+    // response.end()
 })
 
 server.post('/mentors', (request, response) => {
 
-    const body = request.body
-    console.log("body", body)
+    // const body = request.body
+    // console.log("body", body)
 
-    response.setHeader("Content-type", "application/json")
-    response.write(JSON.stringify({mensajePost: "Aqui podras crear un mentor"}))
-    response.end()
+    // response.setHeader("Content-type", "application/json")
+    // response.write(JSON.stringify({mensajePost: "Aqui podras crear un mentor"}))
+    // response.end()
+
+
+
+
 })
-
 
 
 server.get('/koders', (request, response) => {
@@ -62,14 +76,14 @@ server.get('/koders', (request, response) => {
 
 server.post('/koders', (request, response) => {
 
-    const nuevosDatos = request.body
+    const nuevosDatos = request.body  //ya viene en formato Json del postman
        
       fs.readFile("koders.json", 'utf8', (error, datosarchivo) => {          
         if (error) throw error;           
             let archivokoders = datosarchivo.toString();   // convierto los datos leidos a un string
-            archivokoders = JSON.parse(archivokoders);   // de json a objetos javascript
+            archivokoders = JSON.parse(archivokoders);   // de STRING json a objetos javascript
             archivokoders.koders.push(nuevosDatos);    // como ya es un objeto javascript, le hago push para agregar el dato de entrada                
-            let nuevoContenido = JSON.stringify(archivokoders);   // de objeto javascript a json
+            let nuevoContenido = JSON.stringify(archivokoders);   // de objeto javascript a json para guardar en el archivo
             fs.writeFile('koders.json',nuevoContenido,function(err){
                 if(err){
                     console.error(err);
