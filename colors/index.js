eeef866f9467e7f1e723623b5128a52187dd2a15
oscar-rh <oscar.rodriguezh@gmail.com>
2021-08-06 18:21:20 -0500

    ejercicios de filtros de koders

---
+++ colors/index.js
@@ -0,0 +1,18 @@
+const  colors = require('colors');
+ 
+console.log(colors.green('hello'));
+console.log(colors.magenta('hello 2'));
+
+
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
+
+
+console.log(colors.green('hello')); // outputs green text
+console.log(colors.red.underline('i like cake and pies')) // outputs red underlined text
+console.log(colors.inverse('inverse the color')); // inverses the color
+console.log(colors.rainbow('OMG Rainbows!')); // rainbow
+console.log(colors.trap('Run the trap')); // Drops the bass
--- miproyectoexpress/index.js
+++ miproyectoexpress/index.js
@@ -57,6 +57,10 @@ server.post('/mentors', (request, response) => {
 
 server.get('/koders', (request, response) => {
 
+    console.log(request.query)     //  koders?generation=10&name=antonio&limit=5                /// QUERY PARAMS
+
+
+
     fs.readFile("koders.json", 'utf8', (error, datos) => {
         if (error) throw error;
         response.json(JSON.parse(datos))
@@ -74,14 +78,14 @@ server.get('/koders', (request, response) => {
 })
 
 
-server.post('/koders', (request, response) => {
+server.post('/kodersorh', (request, response) => {
 
     const nuevosDatos = request.body  //ya viene en formato Json del postman
        
       fs.readFile("koders.json", 'utf8', (error, datosarchivo) => {          
         if (error) throw error;           
             let archivokoders = datosarchivo.toString();   // convierto los datos leidos a un string
-            archivokoders = JSON.parse(archivokoders);   // de STRING json a objetos javascript
+            archivokoders = JSON.parse(archivokoders);   // de STRING en formato json a objetos javascript
             archivokoders.koders.push(nuevosDatos);    // como ya es un objeto javascript, le hago push para agregar el dato de entrada                
             let nuevoContenido = JSON.stringify(archivokoders);   // de objeto javascript a json para guardar en el archivo
             fs.writeFile('koders.json',nuevoContenido,function(err){
@@ -96,9 +100,6 @@ server.post('/koders', (request, response) => {
 
 
 })
-
-
-
 
 server.listen(8080,()=>{
     console.log("listening on port 8080")
@@ -125,7 +126,8 @@ function readFilePromise( pathToRead )
                 else 
                 {   
                     /* const fileObject = {  pathToRead, content  }*/
-                    resolve(content)
+                    const json = JSON.parse(content)
+                    resolve(json)
                 }           
             }          
             )
@@ -133,7 +135,10 @@ function readFilePromise( pathToRead )
     )
 }
 
-server.get('/read', (req, res) => 
+
+
+//// USANDO PROMESAS
+server.get('/koders', (req, res) => 
 {
     readFilePromise('koders.json')
     .then( (content) =>{
@@ -144,3 +149,201 @@ server.get('/read', (req, res) =>
         res.status(400).json({error: "No se pudo leer el archivo"})
     })
 })
+
+//// USANDO ASYN Y AWAIT, AWAIT HACE QUE LA FUNCION SE ESPERE HASTA QUE TERMINE PARA QUE NO CONTINUE, PERO REQUIERE QUE LA FUNCION SEA ASYNCRONA, 
+/// POR ESO SE LE COLOCA EL ASYNC AL CALLBACK
+server.get('/koders2', async (req, res) => 
+{
+    //const content = await fs.promises.readFile('koders.json', 'utf8') //// ESTA ES UNA OPCION DIFERENTE DE USAR UN METODO DE FS QUE YA DEVUELVE UNA PROMESA
+    const content = await readFilePromise('koders.json', 'utf8')
+    console.log(content)
+    res.status(200).json({success: true  ,
+                          message: "all koders", 
+                          data: {
+                                     koders : content.koders
+                                } 
+                            })
+})
+
+
+/// AGREGA UN KODER 
+server.post('/koders2', async (request, response) => {
+    const newKoder = request.body
+    const content = await readFilePromise('koders.json', 'utf8')
+    content.koders.push(newKoder)
+    fs.writeFileSync ('koders.json', JSON.stringify(content, null , 2), 'utf-8')
+
+    response.json({
+        success : true,
+        message: "Koder agregado",
+        data: {
+            koder: newKoder
+              }
+    })
+
+})
+
+/// PATH PARAMS     /koders/:id
+
+server.patch('/koders/:id', async (request,response) =>{
+    // const id = (request.params.id)       // ----- una forma de tener los datos del objeto 
+    const {id} = request.params 
+    console.log(id)
+    const {name , generation } = request.body          
+    const content = await readFilePromise('koders.json', 'utf8')  
+
+
+    const newKoders = content.koders.map((koder) => {
+
+        if (koder.id === parseInt(id))
+        {
+            koder = {...koder, name, generation}
+        }
+        return koder
+    })
+
+    content.koders = newKoders
+    fs.writeFileSync ('koders.json', JSON.stringify(content, null , 2), 'utf-8')
+
+    response.json({
+            success : true,
+            message : "koder updated"
+
+    })
+
+} )
+
+server.get('/koders/:id' , async (request, response) => {
+
+    const {id} = request.params      
+    const content = await readFilePromise('koders.json', 'utf8')  
+
+    const oneKoder = content.koders.filter(koder => {
+
+        if (koder.id === parseInt(id))
+        {
+            return koder    
+        }    
+        
+    }) 
+    
+    //// otra manera de hacerlo
+    const koderFound = content.koders.find( koder => koder.id === parseInt(id) )
+
+
+    if (!koderFound)
+    {
+        response.json({
+            success : false,
+            message : "koder no encontrado" 
+        })
+
+    }
+    else
+    {
+        response.json({
+            success : true,
+            message : "koder encontrado" ,
+            data    :  oneKoder   ,
+            data2   :  koderFound
+        })
+    }
+
+})
+
+
+server.delete('/koders/:id' , async (request, response)  => {
+
+    const {id} = request.params      
+    const content = await readFilePromise('koders.json', 'utf8')  
+    
+    const newKoders = content.koders.filter( koder => {
+        if (koder.id !== parseInt(id))
+        {
+            return { koder }
+        }
+        
+    } )
+
+    content.koders = newKoders
+    fs.writeFileSync ('koders.json', JSON.stringify(content, null , 2), 'utf-8')
+
+    response.json({
+            success : true,
+            message : "koder borrado"
+
+    })
+
+})
+
+//  koders?generation=10&name=antonio&limit=5                /// QUERY PARAMS
+server.get('/koders4', async (request, response) => {
+
+    // console.log(request.query)     
+    const {generation, gender, name } = request.query
+    const content = await readFilePromise('koders.json', 'utf8')  
+    //console.log(generation, gender, name)
+    let kodersByGeneration = null
+
+    if ( gender && name)
+    {        
+        kodersByGeneration = content.koders.filter( koder =>  koder.gender === gender && koder.name === name  )
+    }    
+    else 
+    {
+            if (generation)
+            {                
+                kodersByGeneration = content.koders.filter( koder =>  koder.generation === parseInt(generation) )
+            }
+
+            if (gender)
+            {             
+                kodersByGeneration = content.koders.filter( koder =>  koder.gender === gender )
+            }    
+
+            if (name)
+            {             
+                kodersByGeneration = content.koders.filter( koder =>  koder.name === name  )
+            }    
+    }
+
+    content.koders = kodersByGeneration  || content.koders
+   
+    response.status(200).json(
+        {
+            success : true, 
+            message : "all koders", 
+            data : {
+                    koders : content.koders
+            }
+        }
+
+    )
+    
+})
+
+
+/*
+
+agregar un dato adicional a koders
+gender:"m"
+
+
+tarea dentro del get koders4: 
+
+1) hacer un filtro por genero 
+2) hacer un filtro por name
+3) si filtro gender =  "m"  y name= "Jose" que haga el filtro correspondiente  
+
+
+
+
+/ vimos: repaso de como realizar peticiones get post 
+/ paso de parametros path params, envia datos dinamicos al endpoint 
+/ query parameters para hacer filtros
+/ Hicimos un crud completo, 
+/ instalamos una dependencia de desarrollo  nodemon -D nodemon , un servicio en 2o plano que checa los cambios de codigo
+
+
+
+*/
